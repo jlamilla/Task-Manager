9776593ee10a491ca9d688315fcaeb6b0eb81300
José David Lamilla <jdavidlamilla@gmail.com>
2024-09-22 23:12:20 -0500

    se realiza el login

--- TaskManagementApp/src/app/UI/main/app-routing.module.ts
+++ TaskManagementApp/src/app/UI/main/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from '../shared/pages/error404-page/error404-page.component';
+import { isNotAuthenticatedGuard } from '../pages/auth/guards/is-not-authenticated.guard';
+import { isAuthenticatedGuard } from '../pages/auth/guards/is-authenticated.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    canActivate: [ isNotAuthenticatedGuard ],
+    loadChildren: () => import('../pages/auth/auth.module').then( m => m.AuthModule ),
+  },
+  {
+    path: '404',
+    component: Error404PageComponent,
+  },
+  {
+    path: 'dashboard',
+    canActivate: [ isAuthenticatedGuard ],
+    loadChildren: () => import('../pages/dashboard/dashboard.module').then( m => m.DashboardModule ),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
